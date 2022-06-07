56e68b4705e0e47d1f701da0f33457ac58f6757b
DRGWeiss <ruizd1710@gmail.com>
2022-06-07 19:48:45 -0400

    images upload with user folder

--- Controllers/ImagesController.cs
+++ Controllers/ImagesController.cs
@@ -1,9 +1,9 @@
 using System;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using System.IO;
 using DRG_Api.Services;
 using Microsoft.AspNetCore.Hosting;
-using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DRG_Api.Controllers
 {
@@ -12,32 +12,46 @@ namespace DRG_Api.Controllers
     public class ImagesController : ControllerBase
     {
         private readonly IUnitOfWork _repositories;
+
         private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ImagesController(IUnitOfWork unitOfWork, IWebHostEnvironment environment)
+        public ImagesController(
+            IUnitOfWork unitOfWork,
+            IWebHostEnvironment environment
+        )
         {
             _repositories = unitOfWork;
             _webHostEnvironment = environment;
         }
 
-
         [HttpPost]
-        public string UploadFile(IFormFile image)
+        public string UploadFile(IFormFile image, string userid)
         {
             string uniqueFileName = null;
+            string uploadsFolder = null;
 
             if (image != null)
             {
-                Console.WriteLine("FILE: " + image.FileName);
-                Console.WriteLine("FOLDER: " + _webHostEnvironment.WebRootPath);
-
-                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "game_images");
-                Console.WriteLine("ROOT: " + uploadsFolder);
+                string imagesFolder =
+                    Path
+                        .Combine(_webHostEnvironment.WebRootPath,
+                        "game_images");
+                if (userid != null)
+                {
+                    uploadsFolder = Path.Combine(imagesFolder, userid);
+                    Directory.CreateDirectory (uploadsFolder);
+                }
+                else
+                {
+                    uploadsFolder = imagesFolder;
+                }
                 uniqueFileName = image.FileName;
                 string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                using (
+                    var fileStream = new FileStream(filePath, FileMode.Create)
+                )
                 {
-                    image.CopyTo(fileStream);
+                    image.CopyTo (fileStream);
                 }
             }
             return uniqueFileName;
