7b3026fe206e9dd5ea1842dc0fa1ffdebf3f9817
DRGWeiss <ruizd1710@gmail.com>
2022-06-27 08:09:18 -0400

    user premium attribute

--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,17 @@
-namespace DRG_Api.Models{
-    public class User {
-        public string userid {get; set;}
-        public string username {get; set;}
-        public string display_name {get; set;}
-        public string password {get; set;}
-        public bool disabled {get; set;}
-        
+namespace DRG_Api.Models
+{
+    public class User
+    {
+        public string userid { get; set; }
+
+        public string username { get; set; }
+
+        public string display_name { get; set; }
+
+        public string password { get; set; }
+
+        public bool disabled { get; set; }
+
+        public bool premium { get; set; }
     }
 }
-
