674d47737291afc6a11a7fcaa899a261ce173840
DRGWeiss <ruizd1710@gmail.com>
2022-07-24 12:18:36 -0400

    steam_page field

--- Models/PlayedGame.cs
+++ Models/PlayedGame.cs
@@ -3,19 +3,37 @@ namespace DRG_Api.Models
     public class PlayedGame
     {
         public string id { get; set; }
+
         public string name { get; set; }
+
         public string year { get; set; }
+
+        public string played_year { get; set; }
+
         public string developer { get; set; }
+
         public string publisher { get; set; }
+
         public string genre { get; set; }
+
         public int rating { get; set; }
+
         public Platform platform { get; set; }
+
         public int platformid { get; set; }
+
         public Status status { get; set; }
+
         public int statusid { get; set; }
+
         public string description { get; set; }
+
         public string image { get; set; }
-        public string userid {get; set;}
-        public User user {get; set;}
+
+        public string userid { get; set; }
+
+        public User user { get; set; }
+
+        public string steam_page { get; set; }
     }
 }
